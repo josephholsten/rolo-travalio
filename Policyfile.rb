23cbd647d07e596fc061e660b38b0afcfa9f9958
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2022-11-08 07:55:02 +0000

    fix base ref

--- Policyfile.rb
+++ Policyfile.rb
@@ -5,6 +5,5 @@ default_source :supermarket
 run_list 'travalio::default'
 
 cookbook 'travalio', path: '.'
-cookbook 'base'
-# cookbook 'base', github: 'josephholsten/role-base'
+cookbook 'base', git: 'https://github.com/josephholsten/role-base.git'
 cookbook 'chef_client_updater', '~> 3.12.3', :supermarket
