41aeca2a8b1e4d6727343f2001d31ecca8b45908
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2023-11-08 17:03:13 -0800

    utensiles: extract bureau

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
 name 'travalio'
-version '0.1.5'
+version '0.1.6'
 
 depends 'base'
--- recipes/utensiles.rb
+++ recipes/utensiles.rb
@@ -1,12 +1,5 @@
 # travalio::utensiles
 
-# improved terminal emulator {
-  # package "xterm"
-  # package "iterm2"  # improved terminal emulator, macos ui
-  # package "hyper"
-# }
-
-
 # stores {
   # package "arq"     # backups
   # package "dropbox" # file store sync
@@ -16,23 +9,12 @@
 
 # password store {
   # package "pass"         # password store, cli
-  # package "1password"    # password store, gui
-  # package "keepassx"     # password store, foss, gui
-  # package "bitwarden"    # password store, foss, gui
+    package "1password-client2"
+  # package "linux-bitwarden-cli"
 
   # package "oath-toolkit"         # otp generator
   # package "pwgen"
 # }
-
-# launcher {
-  # package "launchbar"    # born:1996
-  # package "quicksilver"  # born:2003-12-02
-  # package "alfred"       # born:2010-03-03
-# }
-
-# ui
-# package "hammerspoon" # window manip ui
-# - gpgtools    # encryption ui
 
 # self-stalking
 # package "imagesnap"      # laptop camera cli
@@ -41,7 +23,6 @@
 # - selfspy
 # - andrewning / sortphotos
 # - nwinter/telepath-logger
-# package "terminal-notifier" 
 
 # botrus dev
 # - bsdiff # binary diff
@@ -50,19 +31,11 @@
 # - tag # osx file tag manip
 # - influxdb # timeseriesdb
 
-
 ## System Administration {{
 
-# package "htop"         # improved top
-# package "appcleaner"   # app removal
-# package "malwarebytes" # antivirus
-# package "flock"        # file locking
-# package "docker"       # containers
-
-# disk analysis {
-  # package "gemini"          # deduplication
-  # package "daisydisk"       # fs usage viz
-# }
+  # package "htop"         # improved top
+  # package "flock"        # file locking
+  # package "docker"       # containers
 
 ## }}
 
@@ -86,17 +59,17 @@
   # tmux-cssh    # born:2017-05-17, -> tmux
 # }
 
-# network file transport {
-  # package "transmission"       # bittorent
-  # package "transmit"           # sftp
-  # package "cyberduck"          # foss sftp
-# }
-
-# VPN clients {
-  # package "viscosity"    # OpenVPN Client
-  # package "tunnelblick"  # OpenVPN Client
-  # package "shimo"        # OpenVPN Client
-  # package "private-internet-access"
+# http client {
+  # curl
+  # wget2
+  # xh
+  # gurl
+  # aria2
+  # hurl
+  # davix
+  # quark
+  # snarf
+  # fetch
 # }
 
 ## }}
@@ -105,14 +78,11 @@
 
 # package "flex"
 # package "bison"
-# package "awk"   # osx ships with 20070501
+# package "awk"     # osx ships with 20070501
 # package "sqlite"
 # package "m4"
-# mas  "Xcode", id: 497799835
 # package "clib"
 # package "cscope"
-# package "dash"         # Documentation Viewer
-
 
 # shell {
   # package "shellcheck" # sh linter # FIXME github.com/koalaman/shellcheck
@@ -120,16 +90,16 @@
 # }
 
 # C {
-# package "splint" # secure programming lint
+  # package "splint" # secure programming lint
 # } 
 
 # ruby {
-# package "ruby"     # osx ships with 2.6.3p62
-# package "ruby-install"
+  # package "ruby"     # osx ships with 2.6.3p62
+  # package "ruby-install"
 # }
 
 # python {
-# package "python"   # osx ships with 3.7.3
+  # package "python"   # osx ships with 3.7.3
 # }
 
 # go {
@@ -143,11 +113,11 @@
 # }
 
 # node {
-# package "node"     # required for npm
+  # package "node"     # required for npm
 # }
 
 # tag search {
-  package "ctags" # osx default is bsd
+    package "ctags" # osx default is bsd
 # }
 
 # version control systems {
@@ -159,7 +129,7 @@
   # package "mercurial"
     package 'git'        # osx ships with 2.3.2, which is ancient
   # package "hub"        # github cli   # FIXME: github.com/github/hub
-  # package "ghq"        # rcs store    # FIXME: github.com/x-motemen/ghq
+    package "ghq"        # rcs store
   # package "hig"        # multi-rcs ui # FIXME: github.com/josephholsten/hig
 # }
 
@@ -167,86 +137,47 @@
 
 ## Writing Tools {{
 
-  # package "graphviz" # diagramming
+# package "graphviz" # diagramming
 
 # improved vi / text editor {
-  package 'vim' do
-    case node[:platform]
-    when 'redhat', 'centos', 'oracle'
-      package_name 'vim-minimal'
-    else
-      package_name 'vim-tiny'
+    package 'vim' do
+      case node[:platform]
+      when 'redhat', 'centos', 'oracle'
+        package_name 'vim-minimal'
+      else
+        package_name 'vim-tiny'
+      end
     end
-  end
   
-  package "neovim"
-
-  # package "visual-studio-code" # graphical text editor
-  # package "atom"             # text editor
-# }
-
-# improved word processor {
-  # mas "Pages", id: 409201541
-  # mas "iA Writer", id: 775737590
+    package "neovim"
+    package "helix"
 # }
 
 # document authoring {
-  # package "sphinx-doc" # reStructuredText
+  # package "sphinx-doc"  # reStructuredText
 # }
 
 # markdown processing {
-  # package "pandoc"          # text document convertion FIXME: pandoc.org
+  # package "pandoc"      # text document convertion FIXME: pandoc.org
   # package "hugo"
   # package "commonmark"
-  # package "qlmarkdown"
-  # package "marked"
 # } 
 
 # html processing {
-  # package "prince"      # html to pdf
-# }
-
-# bibliography management {
-  # package "papers"             # biblio mgmt
+  # package "prince"  # html to pdf
 # }
 
 # }}
 
-# slide deck presentation {
-  # mas  "Keynote", id: 409183694
-# }
-
-# spreadsheets {
-  # mas  "Numbers", id: 409203825
-# }
-
-
 # task management {
   # package "todoman"
-  # package "task"                 # taskwarrior
-  # mas  "Todoist", id: 585829637 # family lists
+  # package "task"    # taskwarrior
   # package 'remind'
   # package "rem"
 # }
 
-# knowledge management {
-  # package "workflowy"    # outlining / external brain
-  # obsidian
-  # roam
-  # dendron
-# }
-
-# audio creation & management {{
-  # package "tuneup"             # music tagging
-  # package "musicbrainz-picard" # music tagging
-  # mas  "GarageBand", id: 682658836
-  # mas  "Audiobook Binder", id: 413969927
-# }}
-
 # image creation & management {{
-  # package "photosweeper-x"
-  # package "mylio"           # photo store
-    package "exiftool" do     # image metadata cli
+    package "exiftool" do  # image metadata cli
       case node[:platform]
       when 'redhat', 'centos', 'oracle'
         package_name 'perl-Image-ExifTool'
@@ -261,43 +192,16 @@
   # package "imagemagick" # image manip
 # }}
 
-# video creation & management {{
-  # package "obs"
-  # mas  "iMovie", id: 408981434
-  # package "handbrake"   # dvd encoding
-# }}
-
 ## Communication {{
 
 # browser {
-  # package "firefox"        # original browser
-
-  # package "brave-browser"  # fallback
-  # package "google-chrome"  # fallback
-  # package "tor-browser"    # anonymous browser, tor + web browser
-  # package "lynx"            # html to text # FIXME
+  # package "lynx"  # html to text # FIXME
+  # w3m
+  # browsh
+  # elinks
+  # links1
 # }
  
-# chat {
-  # package "slackcat"
-  # package "franz"    # chat ui, instant messaging
-  # package "adium"    # instant messaging
-  # package "textual"  # irc, macos gui
-  # package "slack"    # sous-chefs, hangops chat
-  # package "discord"
-  # package "signal"   # secure messaging
-# }
-
-# video chat {
-  # package "skype"
-  # package "zoomus"  # video chat
-# }
-
-# news {
-  # package "panic-unison"  # usenet
-  # package "netnewswire"
-# }
-
 # mail sync {
   # package "offlineimap"  # mail store sync # FIXME
 # }
@@ -320,7 +224,7 @@
 ## }}
 
 # games {
-    package "fortune" do     # image metadata cli
+    package "fortune" do
       case node[:platform]
       when 'redhat', 'centos', 'oracle'
         package_name 'fortune-mod'
@@ -331,27 +235,11 @@
         action :nothing
       else
         # Not supported
+        action :nothing
       end
     end
-
-  # package "sabaki"   # go board
-  # package "steam"    # gaming marketplace
   # package "nethack"  # the game  
-  # package "battle-net"
 # }
 
-
 # package "gnu-typist"         # typing lessons
-
-# package "anki"
-# package "calibre"            # ebook mgmt
-# package "hasklig"            # ligatures 
-# package "electric-sheep"     # the screensaver
-# package "family-tree-maker"  # geneology store
-# package "flux"
-# package "karabiner-elements" # key bindings
-# package "keepingyouawake"    # prevent sleep events
 # package "qrencode"
-
-# mas  "Timer", id: 799574890
-
